2d8a5376012b4115c8131e3c2e1078b7a1614e8e
Arkadiusz Lach <lach@includespark.eu>
2019-07-03 23:12:46 +0200

    Fixed Explosion on Kill dealing damage to players

--- upgrade_system_core.lua
+++ upgrade_system_core.lua
@@ -1,6 +1,6 @@
 dofile("data/upgrade_system_const.lua")
 
-local UPGRADE_SYSTEM_VERSION = "2.3.7"
+local UPGRADE_SYSTEM_VERSION = "2.3.8"
 print(">> Loaded Upgrade System v" .. UPGRADE_SYSTEM_VERSION)
 
 US_CONDITIONS = {}
